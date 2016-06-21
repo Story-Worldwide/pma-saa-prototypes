45fca8917fbfeedf852574f3829a85b823c31c28
Fran Devinney <fran.devinney@Fran-Devinneys-Air.local>
2016-06-21 16:29:39 -0400

    added a new deities page prototype

--- public/js/deities-nav.js
+++ public/js/deities-nav.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	 console.log('ready');
-	 var dnav = $('.deity-nav-item');
+	 var dnav = $('.deity-nav-item'),
+	 	 dname = $('.deity-name-hover-test');
 
 	 $(dnav).each(function(){
 
@@ -9,14 +10,26 @@ $(document).ready(function(){
 	 			// do nothing
 	 		}else{
 	 			$(this).toggleClass('deity-nav-item-hover');
+	 			//dname.removeClass('deity-name-active');
+	 			dname.removeClass('deity-name-active');
 	 		}
 	 	})
 
 	 	$(this).click(function(e){
 	 		e.preventDefault();
+	 		//dname.fadeOut();
+	 		
+	 		dname.addClass('deity-name-active');
 	 		$(dnav).removeClass('deity-nav-item-active');
 	 		$(dnav).removeClass('deity-nav-item-hover');
 	 		$(this).addClass('deity-nav-item-active');
+
+	 		//console.log();
+	 		var newDeityName = $(this).attr('data-deity-name');
+		    setTimeout(function(){
+			  //alert("Boom!");
+			  dname.html(newDeityName);
+			}, 350);
 	 	})
 	 });
 
